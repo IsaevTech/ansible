a1d99a5c0d5305e04d1277ff0b4e8de3b7983535
Mikhail Isaev <isaevmv@it.mos.ru>
2022-10-17 11:48:54 +0300

    reorganize repo

--- debian-11-repos/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ansible-playbook --vault-password-file=./.vault --inventory=./inv 01-apt-repos.yml
-case "$1" in
-"update") ansible-playbook --vault-password-file=./.vault --inventory=./inv 02-update-packages.yml ;;
-esac
